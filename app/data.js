4e2db306c7a9377a3b1a8f0c211fb4f677f1d32e
Hayatul Karima <hayatulkarimah0@gmail.com>
2026-09-10 11:32:15 +0800

    Tambah halaman dan components

---
+++ app/data.js
@@ -0,0 +1,30 @@
+export const kendaraan = [
+    {
+        id:1,
+        nama: "Toyota Avanza",
+        jenis: "MVP",
+        harga: 500000,
+        status: "Tersedia",
+        gambar: "https://th.bing.com/th/id/OIP.JkXxOamLWYXuyKSHuWWRFAHaES?w=323&h=187&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3",
+    },
+
+    {
+        id:2,
+        nama: "Toyota Avanza",
+        jenis: "MVP",
+        harga: 500000,
+        status: "Tidak Tersedia",
+        gambar: "https://th.bing.com/th/id/OIP.JkXxOamLWYXuyKSHuWWRFAHaES?w=323&h=187&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3",
+    }
+
+     ,{
+        id:3,
+        nama: "Toyota Avanza",
+        jenis: "MVP",
+        harga: 500000,
+        status: "Tersedia",
+        gambar: "https://th.bing.com/th/id/OIP.EoVPUNhE71jZ4PCwkEW_8AHaEK?w=304&h=180&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3",
+    }
+
+   
+];
